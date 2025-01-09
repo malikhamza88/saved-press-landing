e152d5e1ea2661cd781482e13f3560bd6d3cb2da
hamzamalik <hm2974022@gmail.com>
2025-01-10 00:10:53 +0500

    - fixed some more issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  base: "/saved-press-landing", // Add your repository name here
+  site: "https://malikhamza88.github.io",
+  base: "/saved-press-landing/",
   integrations: [tailwind()],
 });
